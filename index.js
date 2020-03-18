571ef0372100b15a2defdd669f6679953d0aa828
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-03-18 12:14:19 -0300

    eslint

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 require('dotenv').config()
 const { MongoClient } = require('mongodb')
 
-async function processOrders(db){
-    const collection = db.collection('orders')
-    orders = await collection.find({}).toArray()
-    orders.map(order => {
-      console.log(order)
-    })
+async function processOrdersQueue (db) {
+  const orders = await db.collection('orders').find({}).toArray()
+  orders.map(processOrder)
 }
 
-async function main() {
+function processOrder (order) {
+  console.log(order)
+}
+
+async function main () {
   const dbUser = process.env.DB_USERNAME
   const dbPass = process.env.DB_PASSWORD
   const dbName = process.env.DB_DATABASE
   const uri = `mongodb://${dbUser}:${dbPass}@mongo:27017/${dbName}`
-  console.log(uri)
   const client = new MongoClient(uri)
 
   try {
-      await client.connect()
+    await client.connect()
 
-      const db = client.db(dbName)
+    const db = client.db(dbName)
 
-      await processOrders(db)
+    await processOrdersQueue(db)
   } catch (e) {
-      console.error(e)
+    console.error(e)
   } finally {
-      await client.close()
+    await client.close()
   }
 }
 
