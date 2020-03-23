29db689125d3b2f04aac22d2bda5c6f9ea998be4
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-03-23 17:12:12 -0300

    dados de acesso ao banco

--- src/database.js
+++ src/database.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
+const dbHost = process.env.DB_HOST
+const dbPort = process.env.DB_PORT
 const dbUser = process.env.DB_USERNAME
 const dbPass = process.env.DB_PASSWORD
 const dbName = process.env.DB_DATABASE
 
-const uri = `mongodb://${dbUser}:${dbPass}@mongo:27017/${dbName}`
+const uri = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
