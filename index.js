ec0f7ca024f7445b8580558549b5f2494b698240
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-03-23 12:17:22 -0300

    default port

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+require('dotenv').config()
 const main = require('./src/main')
+
 main().catch(console.error)
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const router = require('./router')
 
 module.exports = async () => {
   try {
-    const port = 3000
+    const port = process.env.APP_PORT
     router.listen(port, () => console.log(`App running on port ${port}!`))
   } catch (e) {
     console.error(e)
